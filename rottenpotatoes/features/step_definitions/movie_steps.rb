388f99c05bda01a810f1832bf2d8d35f5c8f2954
Daniel Kang <hk3021@columbia.edu>
2020-04-16 19:30:30 -0400

    finished Part4

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -18,7 +18,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  temp = page.body.split(e2)
+  expect(temp[0].include? e1).to eq true
 end
 
 # Make it easier to express checking or unchecking several boxes at once
